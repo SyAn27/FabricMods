6bd4114ad45c8baa2c7974e31a664a07946fc839
NinjaPhenix <ninjaphenix@hotmail.co.uk>
2020-03-12 23:49:40 +0000

    Add torcherino (empty)

---
+++ make_link.py
@@ -0,0 +1,18 @@
+import os
+import argparse
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Utility to create sym-links needed for mods.')
+    parser.add_argument("mod_id")
+
+    #args = parser.parse_args()
+    args = parser.parse_args(["torcherino"])
+
+    if not os.path.exists(args.mod_id):
+        os.mkdir(args.mod_id)
+    os.chdir(args.mod_id)
+    os.system("mklink settings.gradle \"..\\base\\settings.gradle\"")
+    os.system("mklink gradlew \"..\\base\\gradlew\"")
+    os.system("mklink gradlew.bat \"..\\base\\gradlew.bat\"")
+    os.system("mklink /D gradle \"..\\base\\gradle\"")
