5128099b22b9bf74e805ad5d12f2eaa48d516022
NinjaPhenix <ninjaphenix@hotmail.co.uk>
2020-03-17 18:58:01 +0000

    - constants.gradle + gradle.properties, towelette and statement in expandedstorage.

--- make_link.py
+++ make_link.py
@@ -12,7 +12,13 @@ if __name__ == "__main__":
         if not os.path.exists(args.mod_id):
             os.mkdir(args.mod_id)
         os.chdir(args.mod_id)
+        print("Linking settings.gradle")
         os.system("mklink settings.gradle \"..\\base\\settings.gradle\"")
+        print("Linking gradlew")
         os.system("mklink gradlew \"..\\base\\gradlew\"")
+        print("Linking gradlew.bat")
         os.system("mklink gradlew.bat \"..\\base\\gradlew.bat\"")
+        print("Linking gradle.properties")
+        os.system("mklink gradle.properties \"..\\base\\gradle.properties\"")
+        print("Linking /gradle/")
         os.system("mklink /D gradle \"..\\base\\gradle\"")
