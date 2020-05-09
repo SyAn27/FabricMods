c143e569ed1aeb332558d2caf0fba1843dc6558b
NinjaPhenix <ninjaphenix@hotmail.co.uk>
2020-05-09 16:23:24 +0100

    Missed some, most can likely be inferred by compiler.

--- chainmail/src/main/java/ninjaphenix/chainmail/api/config/JanksonConfigParser.java
+++ chainmail/src/main/java/ninjaphenix/chainmail/api/config/JanksonConfigParser.java
@@ -30,7 +30,7 @@ public final class JanksonConfigParser
     private static final Logger LOGGER = LogManager.getLogger("chainmail-config");
     private final Jankson _jankson;
 
-    public JanksonConfigParser(Jankson jankson) { _jankson = jankson; }
+    public JanksonConfigParser(final Jankson jankson) { _jankson = jankson; }
 
     /**
      * Attempts to load the config from the specified path.
@@ -40,13 +40,13 @@ public final class JanksonConfigParser
      * @param marker Marker for logging with log4j.
      * @return The loaded config.
      */
-    public <F> F load(Class<F> configClass, Path configPath, Marker marker)
+    public <F> F load(final Class<F> configClass, final Path configPath, final Marker marker)
     {
         final Path folder = configPath.getParent();
         if (Files.notExists(folder))
         {
             try { Files.createDirectories(folder); }
-            catch (IOException e)
+            catch (final IOException e)
             {
                 throw new RuntimeException(MessageFormat.format("[{0}] Cannot create directories required for config.", marker.getName()), e);
             }
@@ -64,11 +64,11 @@ public final class JanksonConfigParser
         {
             return _jankson.fromJson(_jankson.load(configStream), configClass);
         }
-        catch (IOException e)
+        catch (final IOException e)
         {
             throw new RuntimeException(MessageFormat.format("[{0}] IO error occurred when loading config.", marker.getName()), e);
         }
-        catch (SyntaxError e)
+        catch (final SyntaxError e)
         {
             throw new RuntimeException(MessageFormat.format("[{0}] Syntax error occurred when loading config.", marker.getName()), e);
         }
@@ -81,7 +81,7 @@ public final class JanksonConfigParser
      * @return TRUE when config failed to save.
      * @since 0.0.5
      */
-    public <F> boolean save(F config, Path configPath, Marker marker)
+    public <F> boolean save(final F config, final Path configPath, final Marker marker)
     {
         try (final BufferedWriter configStream = Files.newBufferedWriter(configPath, StandardCharsets.UTF_8,
                 StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.CREATE))
@@ -97,7 +97,7 @@ public final class JanksonConfigParser
     }
 
     /* Makes default instance of a config. */
-    private <F> F makeDefault(Class<F> configClass, Marker marker)
+    private <F> F makeDefault(final Class<F> configClass, final Marker marker)
     {
         try { return configClass.newInstance(); }
         catch (final InstantiationException | IllegalAccessException e)
@@ -113,8 +113,8 @@ public final class JanksonConfigParser
 
         public Builder() { _builder = new Jankson.Builder(); }
 
-        public <A, B> Builder registerDeSerializer(Class<A> from, Class<B> to, DeserializerFunction<A, B> deserializer,
-                BiFunction<B, Marshaller, JsonElement> serializer)
+        public <A, B> Builder registerDeSerializer(final Class<A> from, final Class<B> to, final DeserializerFunction<A, B> deserializer,
+                final BiFunction<B, Marshaller, JsonElement> serializer)
         {
             _builder.registerSerializer(to, serializer);
             _builder.registerDeserializer(from, to, deserializer);
