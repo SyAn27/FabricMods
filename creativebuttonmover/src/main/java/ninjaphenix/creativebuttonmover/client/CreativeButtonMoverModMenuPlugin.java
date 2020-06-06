1c00a3e2bb0d489262c0a9cc9a52ede6aa07e346
NinjaPhenix <ninjaphenix@hotmail.co.uk>
2020-06-06 15:16:08 +0100

    Pull in some minor changes from more container screen branch.

--- creativebuttonmover/src/main/java/ninjaphenix/creativebuttonmover/client/CreativeButtonMoverModMenuPlugin.java
+++ creativebuttonmover/src/main/java/ninjaphenix/creativebuttonmover/client/CreativeButtonMoverModMenuPlugin.java
@@ -1,10 +1,8 @@
 package ninjaphenix.creativebuttonmover.client;
 
+import io.github.prospector.modmenu.api.ConfigScreenFactory;
 import io.github.prospector.modmenu.api.ModMenuApi;
-import net.minecraft.client.gui.screen.Screen;
 import ninjaphenix.creativebuttonmover.client.gui.screen.SimulatedCreativeScreen;
-
-import java.util.function.Function;
 
 public class CreativeButtonMoverModMenuPlugin implements ModMenuApi
 {
@@ -12,5 +10,5 @@ public class CreativeButtonMoverModMenuPlugin implements ModMenuApi
     public String getModId() { return "creativebuttonmover"; }
 
     @Override
-    public Function<Screen, ? extends Screen> getConfigScreenFactory() { return SimulatedCreativeScreen::new; }
+    public ConfigScreenFactory<?> getModConfigScreenFactory() { return SimulatedCreativeScreen::new; }
 }
