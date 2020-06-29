f19824d7451e5602fa6c70ecc9d70eaa65732969
NinjaPhenix <ninjaphenix@hotmail.co.uk>
2020-06-29 20:25:13 +0100

    Front chest model finalised.

--- expandedstorage/src/main/java/ninjaphenix/expandedstorage/client/models/FrontChestModel.java
+++ expandedstorage/src/main/java/ninjaphenix/expandedstorage/client/models/FrontChestModel.java
@@ -9,11 +9,11 @@ public class FrontChestModel extends SingleChestModel
     public FrontChestModel()
     {
         // LongChestModel
-        super(96, 80);
+        super(64, 48);
         lid.addCuboid(0, 0, 15, 14, 5, 15, 0);
         lid.addCuboid(6, -2, 30, 2, 4, 1, 0);
         lid.setPivot(1, 9, -15);
-        base.setTextureOffset(0, 35);
+        base.setTextureOffset(0, 20);
         base.addCuboid(0, 0, 0, 14, 10, 15, 0);
         base.setPivot(1, 0, 0);
     }
