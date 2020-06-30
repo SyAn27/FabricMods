e03d6331063a290ab46a521a457fd00b9a24603b
NinjaPhenix <ninjaphenix@hotmail.co.uk>
2020-06-30 14:24:26 +0100

    Right model finalised.

--- expandedstorage/src/main/java/ninjaphenix/expandedstorage/client/models/RightChestModel.java
+++ expandedstorage/src/main/java/ninjaphenix/expandedstorage/client/models/RightChestModel.java
@@ -8,8 +8,7 @@ public class RightChestModel extends SingleChestModel
 {
     public RightChestModel()
     {
-        // VanillaChestModel
-        super(96, 48);
+        super(64, 48);
         lid.addCuboid(0, 0, 0, 15, 5, 14, 0);
         lid.addCuboid(0, -2, 14, 1, 4, 1, 0);
         lid.setPivot(0, 9, 1);
